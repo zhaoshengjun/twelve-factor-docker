c9cc22b573efe3a5611441b490fa4ed9e9bacf6f
Sam Zhao <zhao.shengjun@gmail.com>
2017-12-08 11:10:55 +1000

    use proxy

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,17 @@
 const path = require('path');
 const express = require('express');
+const proxy = require('express-http-proxy');
+const baseImageUrl = process.env.BASE_IMAGE_URL;
+const proxyBaseImageUrl = baseImageUrl
+  ? proxy(baseImageUrl, {
+      proxyReqPathResolver: req => {
+        const newPath = baseImageUrl + req.path;
+        console.log(newPath);
+        return newPath;
+      }
+    })
+  : express.static(path.join(__dirname, 'public/images'));
 const app = express();
 
-app.use('/images', express.static(path.join(__dirname, 'public/images')));
+app.use('/images', proxyBaseImageUrl);
 spp.listen(8080);
